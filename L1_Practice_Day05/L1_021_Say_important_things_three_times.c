28da588ba039c31b7a96f3c4622f3772b05cfae5
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-01-24 15:21:59 +0800

    lhw commit

---
+++ L1_Practice_Day05/L1_021_Say_important_things_three_times.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=0; i<3; i++){
+        printf("I'm gonna WIN!\n");
+    }
+
+
+    /*
+    L1-021 重要的话说三遍
+    分数 5
+    作者 陈越
+    单位 浙江大学
+    这道超级简单的题目没有任何输入。
+    你只需要把这句很重要的话 —— “I'm gonna WIN!”——连续输出三遍就可以了。
+    注意每遍占一行，除了每行的回车不能有任何多余字符。
+    输入样例：
+    无
+    输出样例：
+    I'm gonna WIN!
+    I'm gonna WIN!
+    I'm gonna WIN!
+    */
+    return 0;
+}
