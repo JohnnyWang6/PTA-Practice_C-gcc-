c3a4566fb96ed66b45672f383848ecc8bee16833
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-01-31 23:14:49 +0800

    lhw commit2 on Jan 31th

---
+++ L1_Practice_Day12/L1_065_Code_nonsense.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    /*
+    L1-065 嫑废话上代码
+    分数 5
+    作者 陈越
+    单位 浙江大学
+    Linux 之父 Linus Torvalds 的名言是：“Talk is cheap. Show me the code.”
+    （嫑废话，上代码）。本题就请你直接在屏幕上输出这句话。
+    输入格式：
+    本题没有输入。
+    输出格式：
+    在一行中输出 Talk is cheap. Show me the code.。
+    输入样例：
+    无
+    输出样例：
+    Talk is cheap. Show me the code.
+    */
+    printf("Talk is cheap. Show me the code.\n");
+    return 0;
+}
