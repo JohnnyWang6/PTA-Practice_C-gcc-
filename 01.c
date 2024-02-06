a046f4b882bf260e73644ea4429a2b4978fe3d91
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-02-06 17:24:37 +0800

    lhw commit8 on Feb 6th

--- 01.c
+++ 01.c
@@ -1,26 +1,14 @@
 #include<stdio.h>
 int main(){
-    int N,M,Q,choose1,choose2,i,j;
-    scanf("%d %d %d",&N,&M,&Q);
-    int line[N+1],row[M+1];
-    long int sum = N*M;
-    int tempLine=N,tempRow=M;
-    for(i=0;i<Q;i++){
-        scanf("%d %d",&choose1,&choose2);
-        if(choose1==0){
-            if(line[choose2]==1){
-                sum-=tempRow;
-                tempLine--;
-                line[choose2]=0;
-            }
-        }else if(choose1==1){
-            if(row[choose2]==1){
-                sum-=tempLine;
-                tempRow--;
-                row[choose2]=0;
-            }
-        }
+    int num[6][7]={0},n,i;
+    // scanf("%d %d %d %d %d %d",&num[0][0],&num[1][0],&num[2][0],&num[3][0],&num[4][0],&num[5][0]);
+    // printf("%d %d %d %d %d %d",num[0][0],num[1][0],num[2][0],num[3][0],num[4][0],num[5][0]);
+    printf("%d\n",num[1][1]);
+    for(i=0;i<6;i++){
+        scanf("%d",&num[i][0]);
     }
-    printf("%d\n",sum);
+    scanf("%d",&n);
+    printf("n=%d\n",n);
+    
     return 0;
 }
