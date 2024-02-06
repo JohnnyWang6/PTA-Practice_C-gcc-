3d50f9dabc1b15b7fbb9f193d02eaec69aba8294
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-02-06 13:50:56 +0800

    lhw commit2 on Feb 6th

---
+++ L1_Practice_Day15/L1_089_Best_Documentation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    /*
+    L1-089 最好的文档
+    分数 5
+    作者 陈越
+    单位 浙江大学
+    有一位软件工程师说过一句很有道理的话：“Good code is its own best documentation.”（好代码本身就是最好的文档）。本题就请你直接在屏幕上输出这句话。
+    输入格式：
+    本题没有输入。
+    输出格式：
+    在一行中输出 Good code is its own best documentation.。
+    输入样例：
+    无
+    输出样例：
+    Good code is its own best documentation.
+    */
+    printf("Good code is its own best documentation.\n");
+    return 0;
+}
