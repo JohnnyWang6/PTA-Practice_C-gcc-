b25e708aa70cb247b0e8eb1d6ebcc0e07556a6e3
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-02-28 22:18:16 +0800

    lhw commit4 on Feb 28th

--- L1_Test2/L1_010_Compare_size.c
+++ L1_Test2/L1_010_Compare_size.c
@@ -13,3 +13,22 @@
     输出样例:
     2->4->8
 */
+#include<stdio.h>
+int main(){
+    int x1, x2, x3, max, min , medium; //定义最大值，最小值和中间值
+    scanf("%d %d %d",&x1,&x2,&x3);
+    max = min = x1;
+    if((x2 > max || x3 > max) && x3 < x2){
+        max = x2;
+    }else if((x2 > max || x3 > max) && x3 > x2){
+        max = x3;
+    }
+    if ((x2 < min || x3 < min) && x3 < x2){
+        min = x3;
+    }else if((x2 < min || x3 < min) && x3 > x2){
+        min = x2;
+    }
+    medium = x1 + x2 + x3 - max - min;
+    printf("%d->%d->%d\n",min, medium, max);
+    return 0;
+}
