b5e76fe8090365eb3b869c7fed4b3aab64555375
JohnnyWang6 <johnnywanglhw@gmail.com>
2024-02-29 17:27:53 +0800

    lhw commit2 on Feb 28th

--- L1_Test2/L1_011_A_B.c
+++ L1_Test2/L1_011_A_B.c
@@ -6,7 +6,7 @@
     本题要求你计算A−B。不过麻烦的是，A和B都是字符串 —— 即从字符串A中把字符串B所包含的字符全删掉，
     剩下的字符组成的就是字符串A−B。
     输入格式：
-    输入在2行中先后给出字符串A和B。两字符串的长度都不超过10 ，并且保证每个字符串都是由可见的ASCII码和空白字符组成，
+    输入在2行中先后给出字符串A和B。两字符串的长度都不超过10的4次方 ，并且保证每个字符串都是由可见的ASCII码和空白字符组成，
     最后以换行符结束。
     输出格式：
     在一行中打印出A−B的结果字符串。
@@ -16,3 +16,20 @@
     输出样例：
     I lv GPLT!  It's  fn gm!
 */
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char A[10001],B[10001];
+    int i,len;
+    gets(A);
+    gets(B);
+    len = strlen(A);
+    for(i=0;i<len;i++){
+        if(strchr(B,A[i])){
+            continue;
+        }else{
+            printf("%c",A[i]);
+        }
+    }
+    return 0;
+}
